7fc158a3bd296a63361ba8d40688c0b0b9b0f74e
Michael Thomson <michael@takoagency.com>
2023-04-14 20:25:22 -0500

    server

--- server.js
+++ server.js
@@ -15,10 +15,26 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/ventures', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/ventures.html'));
+});
+
+app.get('/vacancies', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/vacancies.html'));
+});
+
+app.get('/contact', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/contact.html'));
+});
+
 app.get('/events', function(req, res) {
     res.sendFile(path.join(__dirname, '/front-end/www.brysonstavern.com/events.html'));
 });
   
-app.listen(8001);
-
-console.log('listening on port 8001!');
+app.listen(8001, () => {
+    console.log('listening on port 8001!');  
+})
